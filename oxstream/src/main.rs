ceef86fa37c665489b580f328b677ccd4e193d20
Miguel Elizalde <miguel.elizalde.19.98@live.com>
2026-03-10 20:36:54 -0600

    Refactor main function to use anyhow for error handling and initialize GStreamer

--- oxstream/src/main.rs
+++ oxstream/src/main.rs
@@ -1,10 +1,24 @@
+use anyhow::{Result, anyhow};
+use gst::Pipeline;
 use tracing::info;
 
-fn main() {
+fn main() -> Result<()> {
     init_tracing();
-    info!("Hello, world!");
+    init_gst()?;
+    info!("GStreamer initialized successfully");
+    let _pipeline = Pipeline::new();
+    info!("Pipeline created successfully");
+    Ok(())
 }
 
 fn init_tracing() {
     tracing_subscriber::fmt::init();
+}
+
+fn init_gst() -> Result<()> {
+    if let Err(err) = gst::init() {
+        tracing::error!("Failed to initialize GStreamer: {}", err);
+        return Err(anyhow!("Failed to initialize GStreamer"));
+    }
+    Ok(())
 }
