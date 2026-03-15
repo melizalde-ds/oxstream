0a3504d4f2871ef496f1dd5666d2520fd3645ae6
Miguel Elizalde <miguel.elizalde.19.98@live.com>
2026-03-14 23:34:08 -0600

    feat: restructure oxstream-signal module and update dependencies

---
+++ signal/src/main.rs
@@ -0,0 +1,49 @@
+use anyhow::Result;
+use axum::extract::WebSocketUpgrade;
+use axum::extract::ws::{Message, WebSocket};
+use axum::response::IntoResponse;
+use axum::{Router, routing::get};
+use futures_util::{sink::SinkExt, stream::StreamExt};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let app = Router::new().route("/", get(ws_handler));
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await?;
+    axum::serve(listener, app).await?;
+    Ok(())
+}
+
+async fn ws_handler(ws: WebSocketUpgrade) -> impl IntoResponse {
+    ws.on_upgrade(handle_socket)
+}
+
+async fn handle_socket(mut socket: WebSocket) {
+    let (mut sender, mut receiver) = socket.split();
+    let (tx, mut rx) = tokio::sync::mpsc::channel::<Message>(32);
+
+    let mut send_task = tokio::spawn(async move {
+        while let Some(msg) = rx.recv().await {
+            if sender.send(msg).await.is_err() {
+                break;
+            }
+        }
+        while let Some(msg) = rx.recv().await {
+            if sender.send(msg).await.is_err() {
+                break;
+            }
+        }
+    });
+
+    let tx2 = tx.clone();
+
+    let mut recv_task = tokio::spawn(async move {
+        while let Some(Ok(msg)) = receiver.next().await {
+            let _ = tx2.send(msg).await;
+        }
+    });
+
+    tokio::select! {
+        _ = &mut send_task => recv_task.abort(),
+        _ = &mut recv_task => send_task.abort(),
+    }
+}
