da2419990f0728f680717760a6368b06541ec9f1
Miguel Elizalde <miguel.elizalde.19.98@live.com>
2026-03-16 13:47:54 -0600

    feat: add tracing and tracing-subscriber for improved logging in the signaling server

--- signal/src/main.rs
+++ signal/src/main.rs
@@ -4,11 +4,20 @@ use axum::extract::ws::{Message, WebSocket};
 use axum::response::IntoResponse;
 use axum::{Router, routing::get};
 use futures_util::{sink::SinkExt, stream::StreamExt};
+use tracing::info;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    init_tracing();
+    info!("Starting Signalling server...");
     let app = Router::new().route("/", get(ws_handler));
+    info!("Router configured successfully");
+
+    info!("Opening TCP listener on 127.0.0.1:8080");
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await?;
+    info!("TCP listener opened successfully on 127.0.0.1:8080");
+
+    info!("Starting Axum server...");
     axum::serve(listener, app).await?;
     Ok(())
 }
@@ -17,7 +26,7 @@ async fn ws_handler(ws: WebSocketUpgrade) -> impl IntoResponse {
     ws.on_upgrade(handle_socket)
 }
 
-async fn handle_socket(mut socket: WebSocket) {
+async fn handle_socket(socket: WebSocket) {
     let (mut sender, mut receiver) = socket.split();
     let (tx, mut rx) = tokio::sync::mpsc::channel::<Message>(32);
 
@@ -46,4 +55,8 @@ async fn handle_socket(mut socket: WebSocket) {
         _ = &mut send_task => recv_task.abort(),
         _ = &mut recv_task => send_task.abort(),
     }
+}
+
+fn init_tracing() {
+    tracing_subscriber::fmt::init();
 }
